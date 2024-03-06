da7194d4d34c20f09752a3eb7f0ee1bb1b300d1c
Paul Borges <theassailant@protonmail.com>
2024-03-06 16:55:40 -0500

    Update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,14 @@
 	<h1 class="font-semibold text-5xl">hey, it's Paul</h1>
 	<h2 class="font-semibold text-3xl">Welcome to my personal site.</h2>
 	<p>
-		Its GitHub repo lives <a
+		The frontend lives <a
 			href="https://github.com/heyitspaul/heyitspaul.dev"
+			class="text-blue-600 hover:underline"
+			target="_blank">here</a
+		>
+		and the backend lives
+		<a
+			href="https://github.com/heyitspaul/heyitspaul.dev-api"
 			class="text-blue-600 hover:underline"
 			target="_blank">here</a
 		>.
