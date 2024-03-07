97c0f4209b7409042f3da1c022d33d641a8a5d21
theassailant <theassailant@protonmail.com>
2024-03-07 14:59:52 -0500

    update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,26 @@
 <div class="text-center">
-    <h1 class="font-semibold text-5xl">hey, it's Paul</h1>
-    <h2 class="font-semibold text-3xl">Welcome to my personal site.</h2>
+    <h1 class="font-semibold text-5xl mb-2">hey, it's Paul</h1>
+    <h2 class="font-semibold text-3xl mb-2">Welcome to my personal site.</h2>
     <p>
-        The frontend lives <a
+        The frontend <a
             href="https://github.com/heyitspaul/heyitspaul.dev"
             class="text-blue-600 hover:underline"
-            target="_blank">here</a
+            target="_blank">is built with SvelteKit</a
         >
-        and the backend lives
+        and the backend
         <a
             href="https://github.com/heyitspaul/heyitspaul.dev-api"
             class="text-blue-600 hover:underline"
-            target="_blank">here</a
+            target="_blank">is built with Ruby on Rails</a
         >.
+    </p>
+    <div class="spacer"></div>
+    <p>I began coding in 2010 with game development using C# with XNA and then Java.</p>
+    <p>I've been writing Ruby, Rails, PHP, and JavaScript code since 2014.</p>
+    <p>I have a passion for web development and design.</p>
+    <div class="spacer"></div>
+    <p>
+        Check out my <a href="/blog" class="text-blue-600 hover:underline">blog</a> for insights into
+        how I develop things.
     </p>
 </div>
