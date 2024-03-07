6aa2cffcb23642fe9c049f814d07b5edc238b47c
Paul Borges <theassailant@protonmail.com>
2024-03-06 21:12:18 -0500

    Add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,33 @@
 <script>
     import '../app.css';
+
+    const menu_links = [
+        {
+            name: 'Home',
+            link: '/'
+        },
+        {
+            name: 'Blog',
+            link: '/blog'
+        },
+        {
+            name: 'Contact',
+            link: '/contact'
+        }
+    ];
 </script>
 
-<slot />
+<div class="container mx-auto">
+    <div class="navbar bg-base-100">
+        <div class="flex-1"></div>
+        <div class="flex-none">
+            <ul class="menu menu-horizontal px-1">
+                {#each menu_links as link}
+                    <li><a href={link.link}>{link.name}</a></li>
+                {/each}
+            </ul>
+        </div>
+    </div>
+
+    <slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,17 @@
 <div class="text-center">
-	<h1 class="font-semibold text-5xl">hey, it's Paul</h1>
-	<h2 class="font-semibold text-3xl">Welcome to my personal site.</h2>
-	<p>
-		The frontend lives <a
-			href="https://github.com/heyitspaul/heyitspaul.dev"
-			class="text-blue-600 hover:underline"
-			target="_blank">here</a
-		>
-		and the backend lives
-		<a
-			href="https://github.com/heyitspaul/heyitspaul.dev-api"
-			class="text-blue-600 hover:underline"
-			target="_blank">here</a
-		>.
-	</p>
+    <h1 class="font-semibold text-5xl">hey, it's Paul</h1>
+    <h2 class="font-semibold text-3xl">Welcome to my personal site.</h2>
+    <p>
+        The frontend lives <a
+            href="https://github.com/heyitspaul/heyitspaul.dev"
+            class="text-blue-600 hover:underline"
+            target="_blank">here</a
+        >
+        and the backend lives
+        <a
+            href="https://github.com/heyitspaul/heyitspaul.dev-api"
+            class="text-blue-600 hover:underline"
+            target="_blank">here</a
+        >.
+    </p>
 </div>
