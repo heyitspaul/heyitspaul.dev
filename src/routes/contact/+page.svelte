e93c4f3f7cfd31c34f15a36517ea5c7690507199
theassailant <theassailant@protonmail.com>
2024-03-07 14:59:45 -0500

    create contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,18 @@
+<div class="text-center">
+    <h1 class="font-semibold text-5xl">Contact</h1>
+    <div class="spacer"></div>
+    <p>
+        My GitHub username is <a
+            href="https://github.com/heyitspaul"
+            class="text-blue-600 hover:underline"
+            target="_blank">heyitspaul</a
+        >.
+    </p>
+    <p>
+        You can also email me with questions or job opportunities at <a
+            href="mailto:paul@heyitspaul.dev"
+            class="text-blue-600 hover:underline"
+            target="_blank">paul@heyitspaul.dev</a
+        >.
+    </p>
+</div>
