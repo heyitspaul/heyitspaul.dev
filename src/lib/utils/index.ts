c240ee9f3677397e02bcd48de7fb99d8a085b5d8
theassailant <theassailant@protonmail.com>
2024-03-14 01:19:05 -0400

    Attempt fix for Vercel

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,11 +1,11 @@
 export const fetch_markdown_posts = async () => {
-    const allPostFiles = import.meta.glob('/src/posts/*.md');
+    const allPostFiles = import.meta.glob('../../../src/posts/*.md');
     const iterablePostFiles = Object.entries(allPostFiles);
 
     const allPosts = await Promise.all(
         iterablePostFiles.map(async ([path, resolver]) => {
             const { metadata } = await resolver();
-            const postPath = "blog/" + path.slice(11, -3);
+            const postPath = "blog" + path.slice(11, -3);
 
             return {
                 meta: metadata,
