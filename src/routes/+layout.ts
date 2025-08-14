193c62f16670690a1701709683b8bbc04181925d
Paul B <paul@heyitspaul.dev>
2025-08-14 16:49:22 -0400

    fix posthog

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -6,9 +6,9 @@ export const load = async () => {
     if (browser) {
         posthog.init(PUBLIC_POSTHOG_KEY, {
             api_host: PUBLIC_POSTHOG_HOST,
-            capture_pageview: false,
-            capture_pageleave: false,
-            capture_exceptions: true // This enables capturing exceptions using Error Tracking, set to false if you don't want this
+            capture_pageview: true,
+            capture_pageleave: true,
+            capture_exceptions: true
         });
     }
 };
--- src/routes/blog/+layout.ts
+++ src/routes/blog/+layout.ts
@@ -1,13 +1,1 @@
-import posthog from 'posthog-js';
-import { browser } from '$app/environment';
-import { env } from '$env/dynamic/public';
-
-export const load = async () => {
-    if (browser) {
-        posthog.init(env.PUBLIC_POSTHOG_API_KEY || '', {
-            api_host: 'https://us.i.posthog.com',
-        });
-    }
-};
-
 export const prerender = true;
