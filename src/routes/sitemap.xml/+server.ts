49f8444d12f9ca6c80dab2b91e20b13db179b3c5
Paul B <paul@heyitspaul.dev>
2025-08-14 16:46:17 -0400

    Adjust sitemap

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -2,11 +2,10 @@ import * as sitemap from 'super-sitemap';
 import { get_blog_slugs } from '$lib/utils';
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async ({ params }) => {
+export const GET: RequestHandler = async () => {
     const blog_slugs = await get_blog_slugs();
     return await sitemap.response({
         origin: 'https://heyitspaul.dev',
-        page: params.slug,
         maxPerPage: 50_000,
         paramValues: {
             '/blog/[slug]': blog_slugs,
